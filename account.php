d50000e9098e307f8df8f603aa92b238f53f92f7
Andrew GALAVAN <agalavan@student.unit.ua>
2018-06-05 15:26:17 +0300

    restructure js files

--- account.php
+++ account.php
@@ -17,52 +17,7 @@ include 'inc/header.php';
 		<button id="snap">Snap Photo</button>
 		<canvas id="canvas" width="640" height="480"></canvas>
 	</div><!--/.primary-->
-	<script type="text/javascript">
-		
-		// Put event listeners into place
-		window.addEventListener("DOMContentLoaded", function() {
-			// Grab elements, create settings, etc.
-            var canvas = document.getElementById('canvas');
-            var context = canvas.getContext('2d');
-            var video = document.getElementById('video');
-            var mediaConfig =  { video: true };
-            var errBack = function(e) {
-            	console.log('An error has occurred!', e)
-            };
-
-			// Put video listeners into place
-            if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-                navigator.mediaDevices.getUserMedia(mediaConfig).then(function(stream) {
-                    video.src = window.URL.createObjectURL(stream);
-                    video.play();
-                });
-            }
-
-            /* Legacy code below! */
-            else if(navigator.getUserMedia) { // Standard
-				navigator.getUserMedia(mediaConfig, function(stream) {
-					video.src = stream;
-					video.play();
-				}, errBack);
-			} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-				navigator.webkitGetUserMedia(mediaConfig, function(stream){
-					video.src = window.webkitURL.createObjectURL(stream);
-					video.play();
-				}, errBack);
-			} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
-				navigator.mozGetUserMedia(mediaConfig, function(stream){
-					video.src = window.URL.createObjectURL(stream);
-					video.play();
-				}, errBack);
-			}
-
-			// Trigger photo take
-			document.getElementById('snap').addEventListener('click', function() {
-				context.drawImage(video, 0, 0, 640, 480);
-			});
-		}, false);
-
-	</script>
+	
 
 	<div class="account_secondary col">
 		<h2>Welcome!</h2>
@@ -72,5 +27,7 @@ include 'inc/header.php';
 	</div><!--/.secondary-->
 	
 </div>
+
+<script src="js/photo.js"></script>
 
 <?php include 'inc/footer.php'; ?>
