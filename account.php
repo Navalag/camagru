4a4b35b30c3a36b6887fffedc5f61c7c7d496454
Andrew <agalavan@student.unit.ua>
2018-06-14 17:15:00 +0300

    add new camera script to account page

--- account.php
+++ account.php
@@ -13,30 +13,35 @@ include 'inc/header.php';
 	
 	<div class="account_primary col">
 		<h2>You Look Grate!</h2>
-		<div class="media"></div>
-		<!-- <video id="video" width="640" height="480"></video>
-		<canvas id="canvas" width="640" height="480"></canvas> -->
-		<button id="start-video">Turn-on Camera</button>
-		<button id="snap">Snap Photo</button>
+		
+		<div class="camera">
+			<video id="video" autoplay>Video stream not available.</video>
+			<button id="startVideo">Turn on Camera</button>
+			<button id="snapPhoto">Take Photo</button>
+			<form action="inc/camera-photo/upload_photo.php" method="post" enctype="multipart/form-data">
+				Select image to upload:
+				<input type="file" name="fileToUpload" id="fileToUpload">
+				<input type="submit" value="Upload Image" name="submit">
+			</form>
+		</div>
+		<canvas id="canvas"></canvas>
 		<button id="effect">Add Frame</button>
 		<button id="save">Save Photo</button>
-		<form action="inc/camera-photo/upload_photo.php" method="post" enctype="multipart/form-data">
-		    Select image to upload:
-		    <input type="file" name="fileToUpload" id="fileToUpload">
-		    <input type="submit" value="Upload Image" name="submit">
-		</form>
+
 	</div><!--/.primary-->
 	
 	<div class="account_secondary col">
+
 		<h2>Welcome!</h2>
-		<p>Cupcake ipsum dolor sit.</p>
-		<p>Cupcake ipsum dolor sit. Amet chocolate cake gummies jelly beans candy bonbon brownie candy. Gingerbread powder muffin. Icing cotton candy. Croissant icing pie ice cream brownie I love cheesecake cookie. Pastry chocolate pastry jelly croissant.</p>
-		<p>Cake sesame snaps sweet tart candy canes tiramisu I love oat cake chocolate bar. Jelly beans pastry brownie sugar plum pastry bear claw tiramisu tootsie roll. Tootsie roll wafer I love chocolate donuts.</p>
-		<p id="pngHolder"></p>
+		<div class="output">
+			<img id="photo" alt="The screen capture will appear in this box."> 
+		</div>
+		<!-- <p id="pngHolder"></p> -->
+
 	</div><!--/.secondary-->
 	
 </div>
 
-<script src="js/camera_handler.js"></script>
+<script src="js/camera_handler_2.js"></script>
 
 <?php include 'inc/footer.php'; ?>
