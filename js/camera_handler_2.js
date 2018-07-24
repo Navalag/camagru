91b0d0b0cdd5beedaf7266b412274e19ada484d1
Andrew GALAVAN <agalavan@student.unit.ua>
2018-07-24 12:34:28 +0300

    clean up and restructure camera_handler script

--- js/camera_handler_2.js
+++ js/camera_handler_2.js
@@ -1,175 +1,197 @@
-// (function() {
-	// The width and height of the captured photo. We will set the
-	// width to the value defined here, but the height will be
-	// calculated based on the aspect ratio of the input stream.
+/*
+** The width and height of the captured photo. We will set the
+** width to the value defined here, but the height will be
+** calculated based on the aspect ratio of the input stream.
+*/
+var width = 640;
+var height = 0;
 
-	var width = 640;    // We will scale the photo width to this
-	var height = 0;     // This will be computed based on the input stream
+/*
+** |streaming| indicates whether or not we're currently streaming
+** video from the camera. Obviously, we start at false.
+*/
+var streaming = false;
 
-	// |streaming| indicates whether or not we're currently streaming
-	// video from the camera. Obviously, we start at false.
+/*
+** The various HTML elements we need to configure or control. These
+** will be set by the startup() function.
+*/
+var video = null;
+var canvas = null;
+var photo = null;
+var startVideo = null;
+var snapPhoto = null;
+var addEffect = null;
+var savePhoto = null;
 
-	var streaming = false;
+const ul = document.getElementById('photo');
 
-	// The various HTML elements we need to configure or control. These
-	// will be set by the startup() function.
+/*
+** When user click on remove button in user photo gallery
+** below script will remove photo from page and from database on server
+*/
+ul.addEventListener('click', (e) => {
+	if (e.target.tagName === 'BUTTON') {
+		const button = e.target;
+		const li = button.parentNode;
+		const ul = li.parentNode;
+		const img_alt = li.childNodes[0].alt;
+		const img = "img_id=" + img_alt;
+		const xhr = new XMLHttpRequest();
 
-	var video = null;
-	var canvas = null;
-	var photo = null;
-	var startVideo = null;
-	var snapPhoto = null;
-	var addEffect = null;
-	var savePhoto = null;
-
-	const ul = document.getElementById('photo');
-
-	function startup() {
-		video = document.getElementById('video');
-		canvas = document.getElementById('canvas');
-		// photo = document.getElementById('photo');
-		startVideo = document.getElementById('startVideo');
-		snapPhoto = document.getElementById('snapPhoto');
-		uploadForm = document.getElementById('upload-form');
-		addEffect = document.getElementById('effect');
-		savePhoto = document.getElementById('save');
-
-		startVideo.addEventListener('click', function() {
-			canvas.style.display = 'none';
-			startVideo.style.display = 'none';
-			uploadForm.style.display = 'none';
-			video.style.display = '';
-			navigator.getMedia = ( navigator.getUserMedia ||
-									navigator.webkitGetUserMedia ||
-									navigator.mozGetUserMedia ||
-									navigator.msGetUserMedia);
-
-			navigator.getMedia(
-				{
-					video: true,
-					audio: false
-				},
-				function(stream) {
-					if (navigator.mozGetUserMedia) {
-						video.mozSrcObject = stream;
-					} else {
-						// video.srcObject = stream;
-						var vendorURL = window.URL || window.webkitURL;
-						video.src = vendorURL.createObjectURL(stream);
-					}
-					video.play();
-				},
-				function(err) {
-					console.log("An error occured! " + err);
-				}
-			);
-
-			video.addEventListener('canplay', function(ev){
-				if (!streaming) {
-					height = video.videoHeight / (video.videoWidth/width);
-				
-					// Firefox currently has a bug where the height can't be read from
-					// the video, so we will make assumptions if this happens.
-				
-					if (isNaN(height)) {
-						height = width / (4/3);
-					}
-				
-					video.setAttribute('width', width);
-					video.setAttribute('height', height);
-					canvas.setAttribute('width', width);
-					canvas.setAttribute('height', height);
-					streaming = true;
-				}
-			}, false);
-		});
-
-		snapPhoto.addEventListener('click', function(ev) {
-			video.style.display = 'none';
-			video.pause();
-			canvas.style.display = '';
-			takePicture();
-			ev.preventDefault();
-		}, false);
-
-		// addEffect.addEventListener('click', function(ev){
-		// 	addFilterOnPhoto();
-		// 	ev.preventDefault();
-		// }, false);
-
-		savePhoto.addEventListener('click', function(ev){
-			saveImage();
-			ev.preventDefault();
-		}, false);
-
-	}
-
-	// Fill the photo with an indication that none has been
-	// captured.
-
-	// Remove clear photo function because no need to save
-	// empty canvas to the collection.
-
-	// function clearphoto() {
-	// 	var context = canvas.getContext('2d');
-	// 	context.fillStyle = "#AAA";
-	// 	context.fillRect(0, 0, canvas.width, canvas.height);
-
-	// 	var data = canvas.toDataURL('image/png');
-	// 	var li = createLI(data);
-	// 	ul.appendChild(li);
-	// }
-	
-	/* 
-	** Capture a photo by fetching the current contents 
-	** of the video and drawing it into a canvas,
-	** then converting that to a PNG format data URL.
-	** By drawing it on an offscreen canvas and then
-	** drawing that to the screen, we can change its size
-	** and/or apply other changes before drawing it.
-	*/
-
-	function takePicture() {
-		var context = canvas.getContext('2d');
-		if (width && height) {
-			canvas.width = width;
-			canvas.height = height;
-			context.drawImage(video, 0, 0, width, height);
-		} 
-		// Remove else condition because no need to save
-		// empty canvas to the collection.
-		// Now it only works when user turn on camera.
-		// else {
-		// 	clearphoto();
-		// }
-	}
-
-	// function addFilterOnPhoto(path) {
-	// 	var context = canvas.getContext('2d');
-
-	// 	var img = new Image();   // Create new img element
-	// 	img.addEventListener('load', function() {
-	// 		context.drawImage(img, 0, 0, width, height);
-	// 	}, false);
-	// 	img.src = path; // Set source path
-	// }
-
-	function saveImage() {
-		var xhr = new XMLHttpRequest();
-		var imgFromCanvas = canvas.toDataURL("image/png");
-		var img = "img=" + imgFromCanvas;
-		xhr.open("POST", "../inc/edit_photo/save_photo.php", true);
+		xhr.open("POST", "../inc/edit_photo/delete_photo.php", true);
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState == 4 && xhr.status == 200) {
 				console.log(xhr.responseText);
-				// photo.setAttribute('src', imgFromCanvas);
-				var li = createLI(imgFromCanvas);
-				ul.appendChild(li);
+				ul.removeChild(li);
 			}
 		};
 		xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
 		xhr.send(img);
 	}
+});
+
+/*
+** Main function in this file.
+** It will be activated after page load.
+*/
+function startup() {
+	video = document.getElementById('video');
+	canvas = document.getElementById('canvas');
+	startVideo = document.getElementById('startVideo');
+	snapPhoto = document.getElementById('snapPhoto');
+	uploadForm = document.getElementById('upload-form');
+	addEffect = document.getElementById('effect');
+	savePhoto = document.getElementById('save');
+
+	/*
+	** Listener for TurnOn Camera button
+	*/
+	startVideo.addEventListener('click', turnOnCamera);
+
+	/*
+	** Listener for TakePhoto button
+	*/
+	snapPhoto.addEventListener('click', function(ev) {
+		video.style.display = 'none';
+		video.pause();
+		canvas.style.display = '';
+		takePicture();
+		ev.preventDefault();
+	}, false);
+
+	/*
+	** Listener for SavePhoto button
+	*/
+	savePhoto.addEventListener('click', function(ev){
+		saveImage();
+		ev.preventDefault();
+	}, false);
+
+}
+
+function turnOnCamera() {
+	canvas.style.display = 'none';
+	startVideo.style.display = 'none';
+	uploadForm.style.display = 'none';
+	video.style.display = '';
+	navigator.getMedia = ( navigator.getUserMedia ||
+							navigator.webkitGetUserMedia ||
+							navigator.mozGetUserMedia ||
+							navigator.msGetUserMedia);
+
+	navigator.getMedia(
+		{
+			video: true,
+			audio: false
+		},
+		function(stream) {
+			if (navigator.mozGetUserMedia) {
+				video.mozSrcObject = stream;
+			} else {
+				// video.srcObject = stream;
+				var vendorURL = window.URL || window.webkitURL;
+				video.src = vendorURL.createObjectURL(stream);
+			}
+			video.play();
+		},
+		function(err) {
+			console.log("An error occured! " + err);
+		}
+	);
+
+	video.addEventListener('canplay', function(ev){
+		if (!streaming) {
+			height = video.videoHeight / (video.videoWidth/width);
+		
+			// Firefox currently has a bug where the height can't be read from
+			// the video, so we will make assumptions if this happens.
+		
+			if (isNaN(height)) {
+				height = width / (4/3);
+			}
+		
+			video.setAttribute('width', width);
+			video.setAttribute('height', height);
+			canvas.setAttribute('width', width);
+			canvas.setAttribute('height', height);
+			streaming = true;
+		}
+	}, false);
+}
+
+/* 
+** Capture a photo by fetching the current contents 
+** of the video and drawing it into a canvas,
+** then converting that to a PNG format data URL.
+**
+** By drawing it on an offscreen canvas and then
+** drawing that to the screen, we can change its size
+** and/or apply other changes before drawing it.
+*/
+function takePicture() {
+	var context = canvas.getContext('2d');
+	if (width && height) {
+		canvas.width = width;
+		canvas.height = height;
+		context.drawImage(video, 0, 0, width, height);
+	}
+}
+
+/*
+** Activate this function when user click on frame or picture button
+** user wants to add on canvas
+*/
+function addFilterOnPhoto(path) {
+	var context = canvas.getContext('2d');
+
+	var img = new Image();   // Create new img element
+	img.addEventListener('load', function() {
+		context.drawImage(img, 0, 0, 640, 480);
+	}, false);
+	img.src = path; // Set source path
+	savePhoto.removeAttribute('disabled');
+}
+
+/*
+** This function add captured photo to side block woth user saved photo
+** also it will add photo
+*/
+function saveImage() {
+	var xhr = new XMLHttpRequest();
+	var imgFromCanvas = canvas.toDataURL("image/png");
+	var img = "img=" + imgFromCanvas;
+	xhr.open("POST", "../inc/edit_photo/save_photo.php", true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			console.log(xhr.responseText);
+			// photo.setAttribute('src', imgFromCanvas);
+			var li = createLI(imgFromCanvas);
+			ul.appendChild(li);
+		}
+	};
+	xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+	xhr.send(img);
 
 	function createLI(src) {
 		function createElement(elementName, property, value) {
@@ -189,40 +211,10 @@
 		appendToLI('button', 'textContent', 'remove');
 		return li;
 	}
-
-	ul.addEventListener('click', (e) => {
-		if (e.target.tagName === 'BUTTON') {
-			const button = e.target;
-			const li = button.parentNode;
-			const ul = li.parentNode;
-			const img_alt = li.childNodes[0].alt;
-			const img = "img_id=" + img_alt;
-			const xhr = new XMLHttpRequest();
-
-			xhr.open("POST", "../inc/edit_photo/delete_photo.php", true);
-			xhr.onreadystatechange = function() {
-				if (xhr.readyState == 4 && xhr.status == 200) {
-					console.log(xhr.responseText);
-					ul.removeChild(li);
-				}
-			};
-			xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
-			xhr.send(img);
-		}
-	});
-
-	// Set up our event listener to run the startup process
-	// once loading is complete.
-	window.addEventListener('load', startup, false);
-// })();
-
-function addFilterOnPhoto(path) {
-	var context = canvas.getContext('2d');
-
-	var img = new Image();   // Create new img element
-	img.addEventListener('load', function() {
-		context.drawImage(img, 0, 0, 640, 480);
-	}, false);
-	img.src = path; // Set source path
-	savePhoto.removeAttribute('disabled');
 }
+
+/*
+** Set up our event listener to run the startup process
+** once loading is complete.
+*/
+window.addEventListener('load', startup, false);
