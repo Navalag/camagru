0949804959f359330569c1e9d0130aeeb1e9d3f3
Andrew GALAVAN <agalavan@student.unit.ua>
2018-07-27 13:54:50 +0300

    refactoring save and delete photo php scripts

--- inc/edit_photo/delete_photo.php
+++ inc/edit_photo/delete_photo.php
@@ -4,26 +4,31 @@ if (!isset($_SESSION)) {
 	session_start();
 }
 include($_SERVER["DOCUMENT_ROOT"]."/config/connect.php");
+include($_SERVER["DOCUMENT_ROOT"]."/inc/functions.php");
 
 /*
 ** DELETE PHOTO FROM SERVER FOLDER
 */
 if (!empty($_GET["img_id"]) || !empty($_GET["pg"])) {
+	$img_id = test_input($_GET["img_id"]);
 	try {
 		$result = $conn->prepare(
-			"SELECT src FROM user_img WHERE img_id=" .
-			$_GET['img_id']);
+			"SELECT src FROM user_img 
+			WHERE img_id = '$img_id'
+			AND user_id = '$_SESSION[userID]' LIMIT 1");
 		$result->execute();
 		$file = $result->setFetchMode(PDO::FETCH_ASSOC);
 		$file = $result->fetchAll();
+		if (empty($file)) {
+			echo "Unable to retrieved results";
+			exit;
+		}
 		$file = str_replace('http://localhost:8080/', '', $file[0]['src']);
 		$file = "../../" . $file;
 		if (!unlink($file)) {
 			echo ("Error deleting $file"."\n");
 		}
-		$sql = "DELETE FROM user_img WHERE img_id=" . 
-				$_GET['img_id'];
-
+		$sql = "DELETE FROM user_img WHERE img_id='$img_id'";
 		$conn->exec($sql);
 	} catch (PDOException $e) {
 		echo "Unable to retrieved results";
--- inc/edit_photo/save_photo.php
+++ inc/edit_photo/save_photo.php
@@ -4,44 +4,43 @@ if (!isset($_SESSION)) {
 	session_start();
 }
 include($_SERVER["DOCUMENT_ROOT"]."/config/connect.php");
+include($_SERVER["DOCUMENT_ROOT"]."/inc/functions.php");
 
 /*
 ** SAVE PHOTO TO SERVER FOLDER
 */
-$img_folder = $_SERVER["DOCUMENT_ROOT"]."/img/uploads/";
-$img_coded = $_POST['img'];
-$img_coded = str_replace('data:image/png;base64,', '', $img_coded);
-$img_coded = str_replace(' ', '+', $img_coded);
-$img = base64_decode($img_coded);
-$name = mktime() . ".png";
-$file = $img_folder . $name;
-file_put_contents($file, $img);
-/*
-** ADD PHOTO LINK TO DATABASE
-*/
-// $user = UserExist($_SESSION['loggued_on_user'], $pdo);
-// $owner_id = $user['uid'];
-$path = "http://localhost:8080/img/uploads/" . $name;
-try {
-	$sql = $conn->prepare("SELECT * FROM `users` 
-				WHERE `username` = '$_SESSION[Username]' LIMIT 1");
-	$sql->execute();
-	$user_info = $sql->setFetchMode(PDO::FETCH_ASSOC);
-	$user_info = $sql->fetchAll();
-	$user_info = $user_info[0];
-	$sql = "INSERT INTO `user_img` (`src`, `user_id`) 
-				VALUES ('$path', '$user_info[id]')";
-	$conn->exec($sql);
-	echo "New record created successfully<br>";
-	}
-catch(PDOException $e)
-	{
-	echo $sql . "<br>" . $e->getMessage();
-	}
-$conn = null;
+if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['img'])) {
+	$img_folder = $_SERVER["DOCUMENT_ROOT"]."/img/uploads/";
+	$img_coded = test_input($_POST['img']);
+	$img_coded = str_replace('data:image/png;base64,', '', $img_coded);
+	$img_coded = str_replace(' ', '+', $img_coded);
+	$img = base64_decode($img_coded);
+	$name = mktime() . ".png";
+	$file = $img_folder . $name;
 
-// $pdo->query("INSERT INTO `images` (`owner_id`, `src`) VALUES
-//     ('$owner_id', '$path')");
-// echo $path;
+	file_put_contents($file, $img);
+	/*
+	** ADD PHOTO LINK TO DATABASE
+	*/
+	$path = "http://localhost:8080/img/uploads/" . $name;
+	try {
+		// $sql = $conn->prepare("SELECT * FROM `users` 
+		// 			WHERE `username` = '$_SESSION[Username]' LIMIT 1");
+		// $sql->execute();
+		// $user_info = $sql->setFetchMode(PDO::FETCH_ASSOC);
+		// $user_info = $sql->fetchAll();
+		// if (empty($user_info)) {
+		// 	echo "Database Error!";
+		// 	exit();
+		// }
+		// $user_info = $user_info[0];
+		$sql = "INSERT INTO `user_img` (`src`, `user_id`) 
+					VALUES ('$path', '$_SESSION[userID]')";
+		$conn->exec($sql);
+	} catch(PDOException $e) {
+		echo $sql . "<br>" . $e->getMessage();
+	}
+}
+$conn = null;
 
 ?>
